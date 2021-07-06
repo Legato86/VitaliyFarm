f6ce4d8a6bcb799e73f3a815eacdfd9788584abb
Legato86 <legato86@gmail.com>
2021-07-06 17:43:57 +0300

    Homework PressFire

---
+++ NewVitaliyFarm/Assets/RW/Scripts/ButtonFire.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonFire : MonoBehaviour
+{
+    [SerializeField] private string Fire;
+    
+
+    
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+        
+    {
+        
+    }
+
+    public void PressAim()
+
+    {
+        
+        Debug.Log("Огонь");
+        
+
+    }
+
+}
+    
