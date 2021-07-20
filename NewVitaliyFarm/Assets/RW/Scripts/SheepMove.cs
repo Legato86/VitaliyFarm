f5f7ee6269edc35d6fb7e2f005ce2b026e08af01
Legato86 <legato86@gmail.com>
2021-07-20 17:18:27 +0300

    HomeWorkSaveSheep

--- NewVitaliyFarm/Assets/RW/Scripts/SheepMove.cs
+++ NewVitaliyFarm/Assets/RW/Scripts/SheepMove.cs
@@ -21,16 +21,31 @@ public class SheepMove : MonoBehaviour
 
     void Update()
     {
-        transform.Translate(moveDirection * moveSpeed * Time.deltaTime);
+        transform.Translate(moveDirection * moveSpeed * Time.deltaTime); 
     }
 
     public void SaveSheep()
     {
         rb.isKinematic = false;
         rb.AddForce(Vector3.up * force);
+        ///////////////////////// тут делать
+        //1. Отключить скорость овце на 0 или как в тракторе 
+        //2. отключить бокс коллайдер овце 
+        //3. отключить гравитацию
+        //4. Спаунить патикл со здвигом над овцой или за овцой
 
         GameObject particle = Instantiate(HeartParticlePrefab, transform.position, HeartParticlePrefab.transform.rotation);  // спауним патикл в позиции овци 
         Destroy(particle, 2f);
         Destroy(gameObject, 0.9f);
     }
+    public void SheepJump() 
+    {
+        rb.isKinematic = false;
+        moveSpeed = 0;
+        rb.AddForce(Vector3.up * force);
+        rb.isKinematic =  true;
+        moveSpeed = 1; 
+
+    }
 }
+
